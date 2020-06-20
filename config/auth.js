f61323db783b3a671d1f70ec8ffcd32070d79478
ChintanRP11 <crpansuriya.11@gmail.com>
2020-06-20 23:31:15 +0530

    ssogoogle
    first

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+  'googleAuth' : {
+      'clientID'      : '421240374988-sgfiaqckkus2maddql54diqb9trbvlo6.apps.googleusercontent.com',
+      'clientSecret'  : '9wNTocU18O84V2IC6dcXFjEV',
+      'callbackURL'   : 'http://localhost:5000/auth/google/callback'
+  }
+
+};
---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb+srv://ChintanRP:Cluster123CRP@cluster0-3l7gm.mongodb.net/ssologin?retryWrites=true&w=majority' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
